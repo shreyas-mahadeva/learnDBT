5d7cdd2a61cfdbbde266e882fe5564307d4a6acf
Shreyas Mahadeva <shreyasmahadev55@gmail.com>
2024-12-05 15:18:32 +0000

    Final

--- models/customers.sql
+++ models/customers.sql
@@ -8,24 +8,13 @@
 
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from jaffle_shop.customers
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
 
